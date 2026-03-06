0a3e07c6eab040263d469782531bc504550b37fc
ruth411 <ruthwikdovala@gmail.com>
2026-03-06 14:34:45 -0500

    fix: sanitize PORT value in start script

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-PORT="${PORT:-8000}"
-exec uvicorn api.main:app --host 0.0.0.0 --port "$PORT"
+PORT_VALUE="${PORT:-8000}"
+case "$PORT_VALUE" in
+  ''|*[!0-9]*)
+    PORT_VALUE="8000"
+    ;;
+esac
+
+exec uvicorn api.main:app --host 0.0.0.0 --port "$PORT_VALUE"
